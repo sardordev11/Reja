9234fcd33bc9d0ff0f7d2216ba5f8ec5f5ae3e2e
Sardor Khadjimatov <sardordev11>
2023-05-08 20:53:27 +0900

    fix

--- server.js
+++ server.js
@@ -15,13 +15,15 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4 Routing code
-app.get("/hello", function (req, res) {
-    res.end(`<h1>Hello World by Sardor</h1>`);
-});
-app.get("/gift", function (req, res) {
-    res.end(`<h1>Siz sovgalar bolimidasiz</h1>`);
+app.post("/create-item", (req, res) => {
+    console.log(req.body);
+    res.json({ test: "success" });
 });
 
+app.get("/", function (req, res) {
+        res.render("harid");
+});    
+    
 const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT, function () {
