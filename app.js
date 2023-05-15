4339a5051ce0c63cec2179b1528175d7195299fc
Sardor Khadjimatov <sardordev11>
2023-05-16 04:46:53 +0900

    feat: CRUD operatsiyalari

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 console.log("Web Serverni boshlash");
 const express = require("express");
-//const res = require("express/lib/response");
+const res = require("express/lib/response");
 const app = express();
 
 const fs = require("fs");
@@ -26,16 +26,38 @@ app.use(express.urlencoded({extended: true}));
 // 3 Views code
 app.set("views", "views");
 app.set("view engine", "ejs");
-
-app.post("/create-item", (req, res) => {});
-
-// 4 Routing code
-app.get("/author", (req, res) => {
-res.render("author", { user: user } );
+// Routing code
+app.post("/create-item", (req, res) => {
+  console.log("user entered /create-item");
+  const new_reja = req.body.reja;
+  db.collection("plans").insertOne({reja: new_reja }, (err, data) => {
+    if(err) {
+      console.log(err);
+      res.end("something went wrong");
+    } else {
+      res.end("succussfully added");
+    }
+  });
 });
 
+
+// app.get("/author", (req, res) => {
+// res.render("author", { user: user } );
+// });
+
 app.get("/", function (req, res) {
-  res.render("reja");
+  console.log("user entered /");
+  db.collection("plans")
+  .find()
+  .toArray((err, data) => {
+    if (err) {
+      console.log(err);
+      res.end("something went wrong");
+    } else {
+      console.log(data);
+      res.render("reja", { items: data });
+    }
+  });
 });    
 
 module.exports = app;
