beaa3071c588b7e8a97adb6796f9287bb9fc447d
Sardor Khadjimatov <sardordev11>
2023-05-16 19:59:11 +0900

    feat: browser.js

--- app.js
+++ app.js
@@ -31,12 +31,8 @@ app.post("/create-item", (req, res) => {
   console.log("user entered /create-item");
   const new_reja = req.body.reja;
   db.collection("plans").insertOne({reja: new_reja }, (err, data) => {
-    if(err) {
-      console.log(err);
-      res.end("something went wrong");
-    } else {
-      res.end("succussfully added");
-    }
+    console.log(data.ops);
+    res.jyson(data.ops[0]);
   });
 });
 
