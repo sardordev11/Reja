51d50b12d437b7523956d4b82ef44f6c9c237c78
Sardor Khadjimatov <sardordev11>
2023-05-18 17:50:01 +0900

    Reja loyihamizni yakunlaymiz

--- app.js
+++ app.js
@@ -1,60 +1,96 @@
-console.log("Web Serverni boshlash");
 const express = require("express");
-const res = require("express/lib/response");
-const app = express();
-
 const fs = require("fs");
 
+const app = express();
+
 let user;
-fs.readFile("database/user.json", "utf-8", (err, data) => {
+fs.readFile("database/user.json", "utf8", (err, data) => {
   if (err) {
     console.log("ERROR:", err);
   } else {
-    user = JSON.parse(data)
+    user = JSON.parse(data);
   }
 });
 
-// MongoDB connect
+// MongoDB call
 const db = require("./server").db();
 
-// 1 Kirish code
+const mongodb = require("mongodb");
+
+// 1
 app.use(express.static("public"));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-// 2 Session
-// 3 Views code
+// 2: Session
+
+// 3: Views
 app.set("views", "views");
 app.set("view engine", "ejs");
-// Routing code
-app.post("/create-item", (req, res) => {
+// BSSR -Backend server side rendering
+
+// 4: Routes
+
+app.post("/create-item", function (req, res) {
   console.log("user entered /create-item");
-  const new_reja = req.body.reja;
-  db.collection("plans").insertOne({reja: new_reja }, (err, data) => {
+
+  console.log(req.body);
+  const newReja = req.body.reja;
+  db.collection("plans").insertOne({ reja: newReja }, (err, data) => {
     console.log(data.ops);
-    res.jyson(data.ops[0]);
+
+    res.json(data.ops[0]);
   });
 });
 
+app.post("/delete-item", function (req, res) {
+  const id = req.body.id;
 
-// app.get("/author", (req, res) => {
-// res.render("author", { user: user } );
-// });
+  db.collection("plans").deleteOne(
+    { _id: new mongodb.ObjectId(id) },
+    function (err, data) {
+      res.json({ state: "succes" });
+    }
+  );
+});
+
+app.post("/edit-item", (req, res) => {
+  const data = req.body;
+  db.collection("plans").findOneAndUpdate(
+    {
+      _id: new mongodb.ObjectID(data.id),
+    },
+    { $set: { reja: data.new_input } },
+    function (err, data) {
+      res.json({ state: "success" });
+    }
+  );
+});
+
+app.post("/delete-all", (req, res) => {
+  if (req.body.delete_all) {
+    db.collection("plans").deleteMany(function () {
+      res.json({ state: "All plans deleted" });
+    });
+  }
+});
+
+app.get("/author", function (req, res) {
+  res.render("author", { user: user });
+});
 
 app.get("/", function (req, res) {
   console.log("user entered /");
   db.collection("plans")
-  .find()
-  .toArray((err, data) => {
-    if (err) {
-      console.log(err);
-      res.end("something went wrong");
-    } else {
-      console.log(data);
-      res.render("reja", { items: data });
-    }
-  });
-});    
+    .find()
+    .toArray((err, data) => {
+      if (err) {
+        console.log(err);
+        res.end("Something went wrong");
+      } else {
+        res.render("reja", { items: data });
+      }
+    });
+});
 
 module.exports = app;
-    
--- public/browser.js
+++ public/browser.js
@@ -1,39 +1,89 @@
-console.log("FrontEnd JS ishga tushdi");
-
 function itemTemplate(item) {
-    return `<li 
-    class="list-group-item list-group-item-info d-flex align-items-center justify-content-between">
-    <span class="item-text">${item.reja}</span>
-    <div>
-        <button 
-        data-id="<${item._id}" 
-        class="edit-me btn btn-secondary btn-sm mr-1">
-            O'zgartirish
-        </button>
-        <button data-id="<${item._id}" 
-        class="delete-me btn btn-danger btn-sm">
-        O'chirish
-        </button>
-    </div>
+    return ` <li
+  class="list-group-item list-group-item-info d-flex align-items-center justify-content-between">
+  <span class="item-text">${item.reja} </span>
+  <div>
+    <button
+      data-id="${item._id}"
+      class="edit-me btn btn-secondary btn-sm mr-1">
+      O'zgartirish
+    </button>
+    <button
+      data-id="${item._id}"
+      class="delete-me btn btn-danger btn-sm">
+      O'chirish
+    </button>
+  </div>
   </li>`;
-}
-
-let createField = document.getElementById("create=field");
-
-document.getElementById("create-form").addEventListener("submit", function (e) {
-   e.preventDefault();
-
-   axios
-   .post("/create-item", { reja: createField.value })
-   .then((response) => {
-    document
-    .getElementById("item-list")
-    .insertAdjacentHTML("beforeend", itemTemplate(response.data))
-    createField.value = "";
-    createField.focus();
-   })
-   .catch(err => {
-    console.log("Iltimos qaytadan harakat qiling!");
-   });
-
-});
+  }
+  
+  let createField = document.getElementById("create-field");
+  
+  document.getElementById("create-form").addEventListener("submit", function (e) {
+    e.preventDefault();
+  
+    axios
+      .post("/create-item", { reja: createField.value })
+      .then((res) => {
+        document
+          .getElementById("item-list")
+          .insertAdjacentHTML("beforeend", itemTemplate(res.data));
+        createField.value = "";
+        createField.focus();
+      })
+      .catch((err) => {
+        console.log("Try again");
+      });
+  });
+  
+  document.addEventListener("click", function (e) {
+    // delete operation
+    if (e.target.classList.contains("delete-me")) {
+      if (confirm("Do you wanna delete")) {
+        axios
+          .post("delete-item", { id: e.target.getAttribute("data-id") })
+          .then((res) => {
+            e.target.parentElement.parentElement.remove();
+          })
+          .catch((err) => {
+            console.log("Please try again");
+          });
+      } else {
+        alert("No button clicked");
+      }
+    }
+    // edit operation
+    if (e.target.classList.contains("edit-me")) {
+      let userInput = prompt(
+        "O'zgartirish kiriting",
+        e.target.parentElement.parentElement.querySelector(".item-text").innerHTML
+      );
+      if (userInput) {
+        axios
+          .post("/edit-item", {
+            id: e.target.getAttribute("data-id"),
+            new_input: userInput,
+          })
+          .then((res) => {
+            e.target.parentElement.parentElement.querySelector(
+              ".item-text"
+            ).innerHTML = userInput;
+          })
+          .catch((err) => {
+            console.log("Something went wrong try again");
+          });
+      }
+    }
+  });
+  
+  document.getElementById("clean-all").addEventListener("click", function () {
+    axios
+      .post("/delete-all", { delete_all: true })
+      .then((res) => {
+        // console.log(res);
+        document.location.reload();
+      })
+      .catch((err) => {
+        console.log("Something went wrong");
+      });
+  });
